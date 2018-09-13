1207e456c020aeb0346ed9e3b210a3e4c201fb20
Student Generic Account <narinyan977@gmail.com>
2018-09-13 21:53:27 +0400

    first commit

---
+++ Homework2.js
@@ -0,0 +1,13 @@
+const a=5;
+const b=10;
+const c="axper";
+const d=a+b;
+const e=b*a;
+
+
+    if(a>b){
+          console.log("du krel es");
+         }else              {console.log("krkin porcir");
+         }
+    console.log(d+e);
+
